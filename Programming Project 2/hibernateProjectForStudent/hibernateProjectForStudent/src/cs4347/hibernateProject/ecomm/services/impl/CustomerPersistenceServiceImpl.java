3c8ef1abc4f0448aa5f165b034116cc9fb4f6897
Moja10 <jeffreyimam@gmail.com>
2017-04-26 14:52:45 -0500

    More changes, only one more method on CustomerImpl

--- Programming Project 2/hibernateProjectForStudent/hibernateProjectForStudent/src/cs4347/hibernateProject/ecomm/services/impl/CustomerPersistenceServiceImpl.java
+++ Programming Project 2/hibernateProjectForStudent/hibernateProjectForStudent/src/cs4347/hibernateProject/ecomm/services/impl/CustomerPersistenceServiceImpl.java
@@ -2,12 +2,14 @@ package cs4347.hibernateProject.ecomm.services.impl;
 
 import java.sql.Date;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import cs4347.hibernateProject.ecomm.entity.Customer;
+import cs4347.hibernateProject.ecomm.entity.Purchase;
 import cs4347.hibernateProject.ecomm.services.CustomerPersistenceService;
 import cs4347.hibernateProject.ecomm.util.DAOException;
 
@@ -16,17 +18,33 @@ public class CustomerPersistenceServiceImpl implements CustomerPersistenceServic
 	@PersistenceContext 
 	public EntityManager em; 
 		
-	/**
-	 */
 	@Override
 	public void create(Customer customer) throws SQLException, DAOException
 	{
+		try {
+			em.getTransaction().begin();
+			em.persist(customer);
+			em.getTransaction().commit();
+		}
+		catch (Exception ex) {
+			em.getTransaction().rollback();
+			throw ex;
+		}
 	}
 
 	@Override
 	public Customer retrieve(Long id) 
 	{
-		return null;
+		try {
+			em.getTransaction().begin();
+			Customer cust= (Customer)em.find(Customer.class, id);
+			em.getTransaction().commit();
+			return cust;
+		}
+		catch (Exception ex) {
+			em.getTransaction().rollback();
+			throw ex;
+		}
 	}
 
 	@Override
@@ -42,7 +60,14 @@ public class CustomerPersistenceServiceImpl implements CustomerPersistenceServic
 	@Override
 	public List<Customer> retrieveByZipCode(String zipCode) throws SQLException, DAOException
 	{
-		return null;
+		if((Integer.parseInt(zipCode) < 0 || Integer.parseInt(zipCode) > 99999)){
+			throw new DAOException("Invalid product ID provided");
+		}
+		List<Customer> custs = (List<Customer>)em.createQuery("from Customer as c where c.address.zipcode = :zip")
+		.setParameter("zip", zipCode)
+		.getResultList();
+
+		return custs;
 	}
 
 	@Override
