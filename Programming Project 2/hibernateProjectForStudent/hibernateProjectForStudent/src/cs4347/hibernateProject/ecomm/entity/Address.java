a3afb9fc76db6244003ca899684565febc6e3922
mathcoprocessor <z3r0n3@gmail.com>
2017-04-26 14:20:01 -0500

    Added ID and Table JPA to Address

--- Programming Project 2/hibernateProjectForStudent/hibernateProjectForStudent/src/cs4347/hibernateProject/ecomm/entity/Address.java
+++ Programming Project 2/hibernateProjectForStudent/hibernateProjectForStudent/src/cs4347/hibernateProject/ecomm/entity/Address.java
@@ -1,5 +1,14 @@
 package cs4347.hibernateProject.ecomm.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "address")
+
 public class Address 
 {
 	private Long id;
@@ -9,11 +18,13 @@ public class Address
 	private String state;
 	private String zipcode;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	public Long getId()
 	{
 		return id;
 	}
-
+	
 	public void setId(Long id)
 	{
 		this.id = id;
